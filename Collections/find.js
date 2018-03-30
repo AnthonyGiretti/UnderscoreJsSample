d36b98ac41b12be8a1920e10ba96fb8bab0142c7
Anthony Giretti <anthony.giretti@gmail.com>
2018-03-30 09:43:16 -0400

    Add method documentation

--- Collections/find.js
+++ Collections/find.js
@@ -1,3 +1,6 @@
+/* where_.where(list, properties) 
+Looks through each value in the list, returning an array of all the values that contain all of the key-value pairs listed in properties. */
+
 var _ = require('underscore');
 
 var value = _.find(["Apple", "Banana", "Grapefruit"], function(value) {
