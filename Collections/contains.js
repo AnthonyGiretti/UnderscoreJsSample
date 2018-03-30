0db297b08ddf886d04573f662735df678723e10f
Anthony Giretti <anthony.giretti@gmail.com>
2018-03-30 09:39:06 -0400

    Add method documentation

--- Collections/contains.js
+++ Collections/contains.js
@@ -1,3 +1,6 @@
+/* contains_.contains(list, value, [fromIndex]) Alias: includes 
+Returns true if the value is present in the list. Uses indexOf internally, if list is an Array. Use fromIndex to start your search at a given index. */
+
 var _ = require('underscore');
 
 var results = _.contains(["John", "Jo", "Johnny"], "Johnny");
