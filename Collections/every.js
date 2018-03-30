7bb8a5c1acd1b885fe1515ca326d554fd1be101a
Anthony Giretti <anthony.giretti@gmail.com>
2018-03-30 09:41:44 -0400

    Add method documentation

--- Collections/every.js
+++ Collections/every.js
@@ -1,3 +1,7 @@
+/* every_.every(list, [predicate], [context]) Alias: all 
+Returns true if all of the values in the list pass the predicate truth test. 
+Short-circuits and stops traversing the list if a false element is found. */
+
 var _ = require('underscore');
 
 var results = _.every([{name: "Doe", firstname: "John"},
