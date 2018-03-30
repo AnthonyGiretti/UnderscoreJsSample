d0ea6bb0adc697816ab708257fd31686a9b53b8b
Anthony Giretti <anthony.giretti@gmail.com>
2018-03-30 09:40:40 -0400

    Add method documentation

--- Collections/each.js
+++ Collections/each.js
@@ -1,3 +1,8 @@
+/* each_.each(list, iteratee, [context]) Alias: forEach 
+Iterates over a list of elements, yielding each in turn to an iteratee function. 
+The iteratee is bound to the context object, if one is passed. Each invocation of iteratee is called with three arguments: (element, index, list). 
+If list is a JavaScript object, iteratee's arguments will be (value, key, list). Returns the list for chaining. */
+
 var _ = require('underscore');
 
 var myFunction = function(element, index, list)
