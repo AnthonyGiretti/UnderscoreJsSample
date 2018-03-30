d749273ba96770e41cbfe946fa125d56a57ac249
Anthony Giretti <anthony.giretti@gmail.com>
2018-03-30 09:42:20 -0400

    Add method documentation

--- Collections/filter.js
+++ Collections/filter.js
@@ -1,3 +1,6 @@
+/* filter_.filter(list, predicate, [context]) Alias: select 
+Looks through each value in the list, returning an array of all the values that pass a truth test (predicate). */
+
 var _ = require('underscore');
 
 var value = _.filter(["Apple", "Banana", "Grapefruit"], function(value) {
