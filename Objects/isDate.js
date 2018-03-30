f5a847bbe88abc480466ea92f6ac4b1c6f90e8fc
Anthony Giretti <anthony.giretti@gmail.com>
2018-03-30 09:26:53 -0400

    Add isDate method sample

---
+++ Objects/isDate.js
@@ -0,0 +1,13 @@
+var _ = require('underscore');
+
+var test1 = new Date();
+var test2 = false;
+var test3 = null;
+var test4 = 5;
+var test5 = undefined;
+
+console.log(_.isDate(test1));
+console.log(_.isDate(test2));
+console.log(_.isDate(test3));
+console.log(_.isDate(test4));
+console.log(_.isDate(test5));
