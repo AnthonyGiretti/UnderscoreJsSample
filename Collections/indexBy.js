67bc704cdecc6291c64f4335b0ee11dda5934a8e
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-02 09:06:49 -0400

    Add comment on indexBy method

--- Collections/indexBy.js
+++ Collections/indexBy.js
@@ -1,3 +1,7 @@
+/* indexBy_.indexBy(list, iteratee, [context]) 
+Given a list, and an iteratee function that returns a key for each element in the list (or a property name), returns an object with an index of each item.
+Just like groupBy, but for when you know your keys are unique.*/
+
 var _ = require('underscore');
 
 var listOfProducts = [
