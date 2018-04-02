ee349fcccd8368491d89529be860a0ccd1df889d
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-02 09:14:14 -0400

    Add comment and more sample on invoke function

--- Collections/invoke.js
+++ Collections/invoke.js
@@ -1,4 +1,10 @@
+/* invoke_.invoke(list, methodName, *arguments) 
+Calls the method named by methodName on each value in the list. Any extra arguments passed to invoke will be forwarded on to the method invocation.*/
+
 var _ = require('underscore');
 
 var value = _.invoke([["Banana", "Apple", "Grapefruit"]], 'sort');
 console.log(value);
+
+var value2 = _.invoke([[5, 1, 7], [3, 2, 1]], 'pop');
+console.log(value2);
