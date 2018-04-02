babf0e2712d7f0e0c061a2a73dffda4277f002e5
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-02 09:31:27 -0400

    Add comment on shuffle function

--- Collections/shuffle.js
+++ Collections/shuffle.js
@@ -1,3 +1,6 @@
+/* shuffle_.shuffle(list) 
+Returns a shuffled copy of the list, using a version of the Fisher-Yates shuffle. */
+
 var _ = require('underscore');
 
 var value = _.shuffle(["Apple", "Banana", "Grapefruit","PineApple"]);
