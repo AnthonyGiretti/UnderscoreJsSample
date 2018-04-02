31a288eb1c89d36511e1db2c162635294f57baa6
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-02 09:34:22 -0400

    Add comment on some function

--- Collections/some.js
+++ Collections/some.js
@@ -1,3 +1,6 @@
+/* some_.some(list, [predicate], [context]) Alias: any 
+Returns true if any of the values in the list pass the predicate truth test. Short-circuits and stops traversing the list if a true element is found.*/
+
 var _ = require('underscore');
 
 var results = _.some([{name: "Doe", firstname: "John"},
