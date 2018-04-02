c9fe863448744c9a1f74f1d36149d1e555413e85
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-02 09:28:48 -0400

    Add comment on reduce function

--- Collections/reject.js
+++ Collections/reject.js
@@ -1,3 +1,6 @@
+/* reject_.reject(list, predicate, [context]) 
+Returns the values in list without the elements that the truth test (predicate) passes. The opposite of filter.*/
+
 var _ = require('underscore');
 
 var results = _.reject([{name: "Doe", firstname: "John"},
@@ -6,4 +9,3 @@ var results = _.reject([{name: "Doe", firstname: "John"},
           {name: "Dep", firstname: "Johnny"}], function(item){ return item.name == "Doe"; });
 
 console.log(results);
-
