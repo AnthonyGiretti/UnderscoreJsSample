a9459b5861d557c16127471426aaa32ea0f97f04
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-02 09:30:26 -0400

    Add comment on sample function

--- Collections/sample.js
+++ Collections/sample.js
@@ -1,3 +1,6 @@
+/* sample_.sample(list, [n]) 
+Produce a random sample from the list. Pass a number to return n random elements from the list. Otherwise a single random item will be returned.*/
+
 var _ = require('underscore');
 
 var value = _.sample(["Apple", "Banana", "Grapefruit","PineApple"]);
