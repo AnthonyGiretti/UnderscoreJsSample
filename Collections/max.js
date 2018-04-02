3396d71a38c517fe8f257c86c7a831c3a690d40e
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-02 09:16:28 -0400

    Add comment on max function

--- Collections/max.js
+++ Collections/max.js
@@ -1,3 +1,7 @@
+/* max_.max(list, [iteratee], [context]) 
+Returns the maximum value in list. If an iteratee function is provided, it will be used on each value to generate the criterion by which the value is ranked. 
+-Infinity is returned if list is empty, so an isEmpty guard may be required. Non-numerical values in list will be ignored.*/
+
 var _ = require('underscore');
 
 var results = _.max([{name: "Doe", firstname: "John", age: 40},
