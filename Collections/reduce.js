042e14f2dadef563af97317d4cdb1709171ba96c
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-02 09:25:24 -0400

    Add comment on reduce method

--- Collections/reduce.js
+++ Collections/reduce.js
@@ -1,3 +1,11 @@
+/* reduce_.reduce(list, iteratee, [memo], [context]) Aliases: inject, foldl 
+Also known as inject and foldl, reduce boils down a list of values into a single value. 
+Memo is the initial state of the reduction, and each successive step of it should be returned by iteratee. 
+The iteratee is passed four arguments: the memo, then the value and index (or key) of the iteration, and finally a reference to the entire list.
+
+If no memo is passed to the initial invocation of reduce, the iteratee is not invoked on the first element of the list. 
+The first element is instead passed as the memo in the invocation of the iteratee on the next element in the list.*/
+
 var _ = require('underscore');
 
 var value = _.reduce(["Apple", "Banana", "Grapefruit"], function(memo, value, index) {
