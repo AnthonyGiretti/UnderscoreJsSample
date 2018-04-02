c5f0dda9b5812284da4727e4362f26949ec047be
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-02 09:26:30 -0400

    Add comment on reduceRight method

--- Collections/reduceRight.js
+++ Collections/reduceRight.js
@@ -1,3 +1,6 @@
+/* reduceRight_.reduceRight(list, iteratee, [memo], [context]) Alias: foldr 
+The right-associative version of reduce. Foldr is not as useful in JavaScript as it would be in a language with lazy evaluation.*/
+
 var _ = require('underscore');
 
 var value = _.reduceRight(["Apple", "Banana", "Grapefruit"], function(memo, value, index, list) {
