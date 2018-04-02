c2e4c65f75f5dbd70d14fab6b9f50d1e0320050b
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-02 09:36:09 -0400

    Add comment on toArray function

--- Collections/toArray.js
+++ Collections/toArray.js
@@ -1,3 +1,6 @@
+/* toArray_.toArray(list) 
+Creates a real Array from the list (anything that can be iterated over). Useful for transmuting the arguments object. */
+
 var _ = require('underscore');
 
 var value = (function(){ return _.toArray(arguments); })("Apple", "Banana", "Grapefruit","PineApple");
