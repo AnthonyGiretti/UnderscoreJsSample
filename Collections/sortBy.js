549c5f7c2b382b584b279f13bf5ac3de367a1f26
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-02 09:35:25 -0400

    Add comment on sortBy function

--- Collections/sortBy.js
+++ Collections/sortBy.js
@@ -1,3 +1,7 @@
+/* sortBy_.sortBy(list, iteratee, [context]) 
+Returns a (stably) sorted copy of list, ranked in ascending order by the results of running each value through iteratee. 
+iteratee may also be the string name of the property to sort by (eg. length).*/
+
 var _ = require('underscore');
 
 var value = _.sortBy([{name: "stallone", firstname: "Sylvester"},
