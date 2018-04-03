f1fee65b4d6c933b4b0ff4355af54738378ddbca
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-03 12:12:24 -0400

    Add comment on initial function

--- Arrays/initial.js
+++ Arrays/initial.js
@@ -1,3 +1,6 @@
+/* initial_.initial(array, [n]) 
+Returns everything but the last entry of the array. Especially useful on the arguments object. Pass n to exclude the last n elements from the result.*/
+
 var _ = require('underscore');
 
 var value = _.initial(["Apple", "Banana", "Grapefruit","PineApple"], 3);
