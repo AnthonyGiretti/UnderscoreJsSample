4aa1fa3da7a3d13cd9244026a680c0a8307b1af2
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-03 12:22:47 -0400

    Add comment on rest function

--- Arrays/rest.js
+++ Arrays/rest.js
@@ -1,3 +1,6 @@
+/* rest_.rest(array, [index]) Aliases: tail, drop 
+Returns the rest of the elements in an array. Pass an index to return the values of the array from that index onward. */
+
 var _ = require('underscore');
 
 var value = _.rest(["Apple", "Banana", "Grapefruit","PineApple"], 2);
