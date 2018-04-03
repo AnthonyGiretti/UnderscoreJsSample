b634198bad4203d22d6bf193a4e4af0a6b993e52
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-03 12:00:27 -0400

    Add comment on flatten method

--- Arrays/flatten.js
+++ Arrays/flatten.js
@@ -1,4 +1,10 @@
+/* flatten_.flatten(array, [shallow]) 
+Flattens a nested array (the nesting can be to any depth). If you pass shallow, the array will only be flattened a single level. */
+
 var _ = require('underscore');
 
 var value = _.flatten(["Apple", "Banana", "Grapefruit","PineApple", ["Orange", "Blood orange"]]);
 console.log(value);
+
+var value2 = _.flatten(["Apple", "Banana", "Grapefruit","PineApple", ["Orange", "Blood orange"]], true);
+console.log(value2);
