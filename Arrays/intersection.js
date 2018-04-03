74cbfa465e25930abc6ef1627b0acfbaba13dbd7
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-03 12:13:15 -0400

    Add comment on intersection function

--- Arrays/intersection.js
+++ Arrays/intersection.js
@@ -1,3 +1,6 @@
+/* intersection_.intersection(*arrays) 
+Computes the list of values that are the intersection of all the arrays. Each value in the result is present in each of the arrays.*/
+
 var _ = require('underscore');
 
 var value = _.intersection(["Apple", "Banana", "Grapefruit","PineApple", "Orange"],["Orange", "Blood orange"]);
