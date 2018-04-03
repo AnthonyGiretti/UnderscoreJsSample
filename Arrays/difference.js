a900808d8ac60251e0d3bb3eb0ea8d11c5c65124
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-03 11:40:47 -0400

    Add comment on difference function

--- Arrays/difference.js
+++ Arrays/difference.js
@@ -1,3 +1,6 @@
+/* difference_.difference(array, *others) 
+Similar to without, but returns the values from array that are not present in the other arrays.*/
+
 var _ = require('underscore');
 
 var value = _.difference(["Apple", "Banana", "Grapefruit","PineApple", "Orange"],["Orange", "Blood orange"]);
