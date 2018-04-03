006cb8158062b45ad6f4b083aded39cf769359f3
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-03 12:28:40 -0400

    Add comment on without function

--- Arrays/without.js
+++ Arrays/without.js
@@ -1,3 +1,6 @@
+/* without_.without(array, *values) 
+Returns a copy of the array with all instances of the values removed. */
+
 var _ = require('underscore');
 
 var value = _.without(["Apple", "Banana", "Grapefruit","PineApple", "Orange", "Blood orange"],"Orange", "Blood orange");
