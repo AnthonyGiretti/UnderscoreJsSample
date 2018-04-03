1c323da2f4838266b2284246b72ffe69f53c8a1f
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-03 12:16:53 -0400

    Add comment on lastIndexOf function

--- Arrays/lastIndexOf.js
+++ Arrays/lastIndexOf.js
@@ -1,3 +1,6 @@
+/* lastIndexOf_.lastIndexOf(array, value, [fromIndex]) 
+Returns the index of the last occurrence of value in the array, or -1 if value is not present. Pass fromIndex to start your search at a given index.*/
+
 var _ = require('underscore');
 
 var value = _.lastIndexOf(["Apple", "Banana", "Grapefruit", "Banana", "PineApple"], "Banana");
