479a5d7307ed23e9bb4960064382b6bce9be0a61
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-03 12:14:57 -0400

    Add comment on last function

--- Arrays/last.js
+++ Arrays/last.js
@@ -1,3 +1,6 @@
+/* last_.last(array, [n]) 
+Returns the last element of an array. Passing n will return the last n elements of the array.*/
+
 var _ = require('underscore');
 
 var value = _.last(["Apple", "Banana", "Grapefruit","PineApple"], 3);
