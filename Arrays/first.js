6dedddde38bf76b84702f6c800aead8b4ec581c3
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-03 11:49:29 -0400

    Add comment on first function

--- Arrays/first.js
+++ Arrays/first.js
@@ -1,3 +1,6 @@
+/* first_.first(array, [n]) Aliases: head, take 
+Returns the first element of an array. Passing n will return the first n elements of the array. */
+
 var _ = require('underscore');
 
 var value = _.first(["Apple", "Banana", "Grapefruit","PineApple"]);
