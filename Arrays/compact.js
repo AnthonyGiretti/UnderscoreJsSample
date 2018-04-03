87cbefa124023252d172eb583b9e253c8c937494
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-03 11:37:25 -0400

    Add comment on compact function

--- Arrays/compact.js
+++ Arrays/compact.js
@@ -1,3 +1,6 @@
+/* compact_.compact(array) 
+Returns a copy of the array with all falsy values removed. In JavaScript, false, null, 0, "", undefined and NaN are all falsy. */
+
 var _ = require('underscore');
 
 var value = _.compact([0, "Banana", false, 100]);
