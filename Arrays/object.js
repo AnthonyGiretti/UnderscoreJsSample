a0138d25cbefd6e9236666526fec455cc66f8d43
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-03 12:19:24 -0400

    Add comment on object function

--- Arrays/object.js
+++ Arrays/object.js
@@ -1,3 +1,6 @@
+/* object_.object(list, [values]) 
+Converts arrays into objects. Pass either a single list of [key, value] pairs, or a list of keys, and a list of values. If duplicate keys exist, the last value wins. */
+
 var _ = require('underscore');
 
 var value = _.object(["Canada","USA","France"], ["Apple", "Grape", "Lemon"]);
