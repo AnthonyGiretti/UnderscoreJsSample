d581d28e2f4630e1e0c0fa5380b87b50e80bbdc1
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-03 12:27:53 -0400

    Add comment on unzip function

--- Arrays/unzip.js
+++ Arrays/unzip.js
@@ -1,3 +1,6 @@
+/* unzip_.unzip(array) 
+The opposite of zip. Given an array of arrays, returns a series of new arrays, the first of which contains all of the first elements in the input arrays, the second of which contains all of the second elements, and so on.*/
+
 var _ = require('underscore');
 
 var value = _.unzip([["Canada", "Apple", "red"], ["USA", "Grape", "yellow"]]);
