ea92f0143defabfb777d31f9193d3e58ee765e13
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-03 12:25:42 -0400

    Add comment on union function

--- Arrays/union.js
+++ Arrays/union.js
@@ -1,3 +1,6 @@
+/* union_.union(*arrays) 
+Computes the union of the passed-in arrays: the list of unique items, in order, that are present in one or more of the arrays. */
+
 var _ = require('underscore');
 
 var value = _.union(["Apple", "Banana", "Grapefruit","PineApple", "Orange"],["Orange", "Blood orange"]);
