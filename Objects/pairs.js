57c33bf03f25a7295cd0ccf55b0725df4e16cea5
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-03 21:42:48 -0400

    Add comment on pairs method

--- Objects/pairs.js
+++ Objects/pairs.js
@@ -1,3 +1,6 @@
+/* pairs_.pairs(object) 
+Convert an object into a list of [key, value] pairs. */
+
 var _ = require('underscore');
 
 var value = _.pairs({firstKey: 'firstValue', secondKey: 'secondValue', thirdKey: 'thirdValue'});
