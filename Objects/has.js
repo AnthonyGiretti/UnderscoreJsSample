63b75c811d0eaff1d3204598bf2407827ff0f49f
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-03 21:03:51 -0400

    Add comment on has function

--- Objects/has.js
+++ Objects/has.js
@@ -1,3 +1,7 @@
+/* has_.has(object, key) 
+Does the object contain the given key? Identical to object.hasOwnProperty(key), but uses a safe reference to the hasOwnProperty function, 
+in case it's been overridden accidentally. */
+
 var _ = require('underscore');
 
 var testObject = {firstKey: 'firstValue', secondKey: 'secondValue', thirdKey: 'thirdValue'};
