9f5b9adf3d611e80e5401fdcf7988cd29c0c3017
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-03 21:04:35 -0400

    Add comment on invert function

--- Objects/invert.js
+++ Objects/invert.js
@@ -1,3 +1,7 @@
+/* invert_.invert(object) 
+Returns a copy of the object where the keys have become the values and the values the keys. 
+For this to work, all of your object's values should be unique and string serializable. */
+
 var _ = require('underscore');
 
 var value = _.invert({firstKey: 'firstValue', secondKey: 'secondValue', thirdKey: 'thirdValue'});
