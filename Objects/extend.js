4fc33dba4ad87e72505f4c10919fdf9e1d735e0f
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-03 20:56:03 -0400

    Add comment on extend function

--- Objects/extend.js
+++ Objects/extend.js
@@ -1,3 +1,8 @@
+/* extend_.extend(destination, *sources) 
+Shallowly copy all of the properties in the source objects over to the destination object, and return the destination object. 
+Any nested objects or arrays will be copied by reference, not duplicated. 
+It's in-order, so the last source will override properties of the same name in previous arguments.*/
+
 var _ = require('underscore');
 
 var testObject = {firstKey: 'firstValue', seccondKey: 'secondValue', thirdKey: 'thirdValue'};
