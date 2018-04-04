7e5332043e8b1e762a2ef732a0839dadd25c46e7
NEXUS\Anthony.Giretti <anthony.giretti@nexusinno.com>
2018-04-04 13:24:17 -0400

    Add compose function sample

---
+++ Functions/compose.js
@@ -0,0 +1,6 @@
+var _ = require('underscore');
+
+var greet    = function(name){ return "hi: " + name; };
+var exclaim  = function(statement){ return statement.toUpperCase() + "!"; };
+var welcome = _.compose(greet, exclaim);
+console.log(welcome('Anthony'));
