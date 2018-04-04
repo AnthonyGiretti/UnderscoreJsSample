66618e777cde3703f0c553c87e862209d9dc6be7
Anthony Giretti <anthony.giretti@gmail.com>
2018-04-03 21:30:13 -0400

    Add comment on ismatch function

--- Objects/ismatch.js
+++ Objects/ismatch.js
@@ -1,3 +1,6 @@
+/* isMatch_.isMatch(object, properties) 
+Tells you if the keys and values in properties are contained in object. */
+
 var _ = require('underscore');
 
 var object = {firstKey: 'firstValue', secondKey: 'secondValue', thirdKey: 'thirdValue'};
